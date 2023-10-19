0cf981eaf94a66a058d9c39c22ca845e347e7071
GDMeira <gabrieldega970@gmail.com>
2023-10-19 02:53:41 -0300

    feat: implementação do endpoint get activities

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,164 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, PrismaPromise, Event } from "@prisma/client";
 import dayjs from "dayjs";
+
 const prisma = new PrismaClient();
 
 async function main() {
-  let event = await prisma.event.findFirst();
-  if (!event) {
-    event = await prisma.event.create({
-      data: {
-        title: "Driven.t",
-        logoImageUrl: "https://files.driven.com.br/images/logo-rounded.png",
-        backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
-        startsAt: dayjs().toDate(),
-        endsAt: dayjs().add(21, "days").toDate(),
-      },
-    });
-  }
+  await prisma.$transaction(async (tx) => {
+    let event = await tx.event.findFirst();
 
-  console.log({ event });
+    if (!event) {
+      event = await tx.event.create({
+        data: {
+          title: "Driven.t",
+          logoImageUrl: "https://files.driven.com.br/images/logo-rounded.png",
+          backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
+          startsAt: dayjs().toDate(),
+          endsAt: dayjs().add(21, "days").toDate(),
+        }
+      });
+    }
+
+    const room = await tx.room.findFirst();
+
+    if (!room) {
+      const room1 = await tx.eventRoom.create({
+        data: {
+          name: "Auditório Principal",
+          eventId: event.id,
+        }
+      });
+
+      const room2 = await tx.eventRoom.create({
+        data: {
+          name: "Auditório Lateral",
+          eventId: event.id,
+        }
+      });
+
+      const room3 = await tx.eventRoom.create({
+        data: {
+          name: "Sala de Workshop",
+          eventId: event.id,
+        }
+      });
+
+      await tx.activity.createMany({
+        data: [
+          // first day
+          {
+            name: "Minecraft: montando o PC ideal",
+            capacity: 30,
+            eventRoomId: room1.id,
+            startsAt: dayjs().add(3, "day").add(9, "hour").toDate(),
+            endsAt: dayjs().add(3, "day").add(10, "hour").toDate(),
+          },
+          {
+            name: "Lol: montando o PC ideal",
+            capacity: 30,
+            eventRoomId: room1.id,
+            startsAt: dayjs().add(3, "day").add(10, "hour").toDate(),
+            endsAt: dayjs().add(3, "day").add(11, "hour").toDate(),
+          },
+          {
+            name: "JS no web development",
+            capacity: 50,
+            eventRoomId: room2.id,
+            startsAt: dayjs().add(3, "day").add(9, "hour").toDate(),
+            endsAt: dayjs().add(3, "day").add(11, "hour").toDate(),
+          },
+          {
+            name: "O Futuro do JAVA",
+            capacity: 40,
+            eventRoomId: room3.id,
+            startsAt: dayjs().add(3, "day").add(9, "hour").toDate(),
+            endsAt: dayjs().add(3, "day").add(10, "hour").toDate(),
+          },
+          {
+            name: "JAVA vs C#",
+            capacity: 40,
+            eventRoomId: room3.id,
+            startsAt: dayjs().add(3, "day").add(10, "hour").toDate(),
+            endsAt: dayjs().add(3, "day").add(11, "hour").toDate(),
+          },
+          // second day
+          {
+            name: "Minecraft: montando o PC ideal",
+            capacity: 30,
+            eventRoomId: room2.id,
+            startsAt: dayjs().add(4, "day").add(9, "hour").toDate(),
+            endsAt: dayjs().add(4, "day").add(10, "hour").toDate(),
+          },
+          {
+            name: "Lol: montando o PC ideal",
+            capacity: 40,
+            eventRoomId: room2.id,
+            startsAt: dayjs().add(4, "day").add(10, "hour").toDate(),
+            endsAt: dayjs().add(4, "day").add(11, "hour").toDate(),
+          },
+          {
+            name: "JS no web development",
+            capacity: 50,
+            eventRoomId: room3.id,
+            startsAt: dayjs().add(4, "day").add(9, "hour").toDate(),
+            endsAt: dayjs().add(4, "day").add(11, "hour").toDate(),
+          },
+          {
+            name: "O Futuro do JAVA",
+            capacity: 40,
+            eventRoomId: room1.id,
+            startsAt: dayjs().add(4, "day").add(9, "hour").toDate(),
+            endsAt: dayjs().add(4, "day").add(10, "hour").toDate(),
+          },
+          {
+            name: "JAVA vs C#",
+            capacity: 40,
+            eventRoomId: room1.id,
+            startsAt: dayjs().add(4, "day").add(10, "hour").toDate(),
+            endsAt: dayjs().add(4, "day").add(11, "hour").toDate(),
+          },
+          // third day
+          {
+            name: "Minecraft: montando o PC ideal",
+            capacity: 30,
+            eventRoomId: room3.id,
+            startsAt: dayjs().add(5, "day").add(9, "hour").toDate(),
+            endsAt: dayjs().add(5, "day").add(10, "hour").toDate(),
+          },
+          {
+            name: "Lol: montando o PC ideal",
+            capacity: 50,
+            eventRoomId: room3.id,
+            startsAt: dayjs().add(5, "day").add(10, "hour").toDate(),
+            endsAt: dayjs().add(5, "day").add(11, "hour").toDate(),
+          },
+          {
+            name: "JS no web development",
+            capacity: 50,
+            eventRoomId: room1.id,
+            startsAt: dayjs().add(5, "day").add(9, "hour").toDate(),
+            endsAt: dayjs().add(5, "day").add(11, "hour").toDate(),
+          },
+          {
+            name: "O Futuro do JAVA",
+            capacity: 50,
+            eventRoomId: room2.id,
+            startsAt: dayjs().add(5, "day").add(9, "hour").toDate(),
+            endsAt: dayjs().add(5, "day").add(10, "hour").toDate(),
+          },
+          {
+            name: "JAVA vs C#",
+            capacity: 50,
+            eventRoomId: room2.id,
+            startsAt: dayjs().add(5, "day").add(10, "hour").toDate(),
+            endsAt: dayjs().add(5, "day").add(11, "hour").toDate(),
+          },
+        ]
+      })
+    }
+
+    return event;
+  })
 }
 
 main()
---
+++ src/services/activities-service.ts
@@ -0,0 +1,9 @@
+import { activitiesRepository } from '@/repositories';
+
+export async function getActivities(){
+  return await activitiesRepository.findActivities();
+}
+
+export const activitiesService = {
+  getActivities,
+};
