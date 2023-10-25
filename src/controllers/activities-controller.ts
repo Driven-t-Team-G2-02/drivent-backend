7040928719b9c2822b0fb00c708e68eb628fc41c
Nathália Brum <natybrum.97@gmail.com>
2023-10-25 06:01:37 -0300

    feat: pega as atividades do usuários

--- src/controllers/activities-controller.ts
+++ src/controllers/activities-controller.ts
@@ -10,6 +10,13 @@ export async function getActivities(req: AuthenticatedRequest, res: Response) {
   res.status(httpStatus.OK).send(activities);
 }
 
+export async function getActivitiesByUser(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const activities = await activitiesService.getActivitiesByUser(userId);
+
+  res.status(httpStatus.OK).send(activities);
+}
+
 export async function signUpUserToActivity(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
   const { activityId } = req.body;
